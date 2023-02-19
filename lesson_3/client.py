225a2bc545e0049aa52da179b7649f6265cc57ea
paul <kpasha11@mail.ru>
2023-02-19 17:09:53 +0300

    lesson_3

---
+++ lesson_3/client.py
@@ -0,0 +1,52 @@
+import sys
+import json
+import socket
+import time
+from utils import *
+
+
+def create_presence(account_name='Guest'):
+    out = {
+        'action': 'presence',
+        'time': time.time(),
+        'user': {
+            'account_name': account_name
+        }
+    }
+    return out
+
+
+def process_ans(message):
+    if 'response' in message:
+        if message['response'] == 200:
+            return '200 : OK'
+        return f'400 : {message["error"]}'
+    raise ValueError
+
+
+def main():
+    try:
+        server_address = sys.argv[1]
+        server_port = int(sys.argv[2])
+        if server_port < 1024 or server_port > 65535:
+            raise ValueError
+    except IndexError:
+        server_address = '127.0.0.1'
+        server_port = 7777
+    except ValueError:
+        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.connect((server_address, server_port))
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    try:
+        answer = process_ans(get_message(transport))
+        print(answer)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ lesson_3/utils.py
@@ -0,0 +1,16 @@
+import json
+
+def get_message(client):
+    encoded_response = client.recv(1024)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode('UTF-8')
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        raise ValueError
+    raise ValueError
+
+def send_message(sock, message):
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode('UTF-8')
+    sock.send(encoded_message)
