7044b7b61a1426ec6f8fc9e3c6767666feffb258
paul <kpasha11@mail.ru>
2023-03-05 20:51:32 +0300

    lesson_7

--- lesson_3/r_client.py
+++ lesson_3/r_client.py
@@ -12,12 +12,9 @@ def echo_client():
     with socket(AF_INET, SOCK_STREAM) as sock:
         sock.connect(ADDRESS)
         while True:
-            msg = input('Ваше сообщение: ')
-            if msg == 'exit':
-                break
-            sock.send(msg.encode('utf-8'))
             data = sock.recv(1024).decode('utf-8')
-            print('Ответ: ', data)
+            if data:
+                print('Ответ: ', data)
 
 
 if __name__ == '__main__':
--- lesson_3/server.py
+++ lesson_3/server.py
@@ -1,87 +1,63 @@
-import json
-from socket import *
 import time
-import sys
-from utils import *
-from log.server_log_config import *
+import select
+from socket import socket, AF_INET, SOCK_STREAM
 
 
-@MyServerLogger()
-def process_client_message(message):
-    if 'action' in message and message['action'] == 'presence' and 'time' in message \
-            and 'user' in message and message['user']['account_name'] == 'Paul':
-        log.info('OK')
-        return {'response': 200}
-    log.critical('Bad request')
-    return {
-        'response': 400,
-        'error': 'Bad Request'
-    }
+def read_requests(r_clients, all_clients):
+    responses = []
 
-
-@MyServerLogger()
-def check_port():
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = 7777
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except IndexError:
-        log.critical('После параметра -\'p\' необходимо указать номер порта.')
-        sys.exit(1)
-    except ValueError:
-        log.critical('В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
-
-    return listen_port
-
-
-@MyServerLogger()
-def check_adress():
-    try:
-        if '-a' in sys.argv:
-            listen_adress = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            listen_adress = ''
-
-    except IndexError:
-        log.critical(
-            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
-        sys.exit(1)
-    return listen_adress
-
-
-@MyServerLogger()
-def handler_message(a):
-    transport = a
-    while True:
-        client, client_adress = transport.accept()
+    for sock in r_clients:
         try:
-            message_from_client = get_message(client)
-            print(message_from_client)
-            response = process_client_message(message_from_client)
-            send_message(client, response)
-            client.close()
-            return 1
-        except (ValueError, json.JSONDecodeError):
-            log.critical('Принято некорретное сообщение от клиента.')
-            client.close()
-            return 1
+            data = sock.recv(1024).decode('utf-8')
+            responses.append(data)
+        except:
+            print('Клиент {} {} отключился'.format(sock.fileno(), sock.getpeername()))
+            all_clients.remove(sock)
+
+    return responses
 
 
-def main():
-    listen_port = check_port()
-    listen_adress = check_adress()
+def write_responses(requests, w_clients, all_clients):
+    for sock in w_clients:
+        try:
+            for request in requests:
+                resp = request.encode('utf-8')
+                sock.send(resp)
+        except Exception as e:
+            print('Клиент {} {} отключился'.format(sock.fileno(), sock.getpeername()))
+            sock.close()
+            all_clients.remove(sock)
 
-    transport = socket(AF_INET, SOCK_STREAM)
-    transport.bind((listen_adress, listen_port))
 
-    transport.listen(5)
+def mainloop():
+    address = ('', 10000)
+    clients = []
 
-    handler_message(transport)
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(address)
+    s.listen(5)
+    s.settimeout(0.2)
+    while True:
+        try:
+            conn, addr = s.accept()
+        except OSError as e:
+            pass
+        else:
+            print('Получен запрос на соединение от %s' % str(addr))
+            clients.append(conn)
+        finally:
+            wait = 100
+            r = []
+            w = []
+            try:
+                r, w, e = select.select(clients, clients, [], wait)
+            except:
+                pass
+
+            requests = read_requests(r, clients)
+            if requests:
+                write_responses(requests, w, clients)
 
 
-if __name__ == '__main__':
-    main()
+print('Start')
+mainloop()
